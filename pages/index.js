75beae0ee625dbcf4470a2fd5552435c8dae4e63
Daniel Steele <danielsteele@hotmail.co.uk>
2021-01-30 12:32:19 +0000

    Add motion, fonts, image and component structure

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>The Game</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <p>hi</p>
+      <Head>
+        <title>The Game</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Image
+          className={styles.background}
+          src="/scapes/home.jpg"
+          alt="Lone man staring out at a broken world"
+          width={1366}
+          height={768}
+        />
+        <motion.h1
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+              transition: {
+                delay: 2,
+                duration: 2,
+              },
+            },
+          }}
+          className={styles.title}
+        >
+          Game
+        </motion.h1>
+      </main>
     </>
-  )
+  );
 }
