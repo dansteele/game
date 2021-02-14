4b12393c546457f440ed4c47a765b96d70fbad56
Daniel Steele <danielsteele@hotmail.co.uk>
2021-02-14 15:41:55 +0000

    Player can now attack

--- pages/battle/index.js
+++ pages/battle/index.js
@@ -1,13 +1,40 @@
 import Image from "next/image";
 import styles from "../../styles/Battle.module.scss";
-import Player from "../../lib/units/player/index";
 import Brute from "../../lib/units/brute/index";
+import Player from "../../lib/units/player/index.js";
+import create from "zustand";
 
-const attack = (kind) => {
-  alert(`Hit with a ${kind} attack`);
+const possibleEnemies = [Brute];
+
+const randomEnemy = () => {
+  return possibleEnemies[Math.floor(Math.random() * possibleEnemies.length)];
 };
 
+const enemyStore = create((set, get) => ({
+  kind: Brute,
+  health: 15,
+  defence: 1,
+  baseDamage: 4,
+
+  receiveDamage: (amount) => {
+    set({ health: get().health - (amount - get().defence) });
+  },
+}));
+
+const playerStore = create((set, get) => ({
+  inBattle: true,
+  health: 12,
+  baseDamage: 4,
+  defence: 2,
+}));
+
 export default function Battle() {
+  const Enemy = enemyStore((state) => state.kind);
+
+  const attack = enemyStore((state) => {
+    return () => state.receiveDamage(2);
+  });
+
   return (
     <>
       <main className={styles.main}>
@@ -20,12 +47,11 @@ export default function Battle() {
           />
         </span>
         <div className={styles.wrapper}>
-          <Player />
+          <Player health={playerStore((state) => state.health)} />
           <div>
-            <button onClick={() => attack("heavy")}>Heavy attack</button>
-            <button onClick={() => attack("light")}>Light attack</button>
+            <button onClick={attack}>Attack</button>
           </div>
-          <Brute />
+          <Enemy health={enemyStore((state) => state.health)} />
         </div>
       </main>
     </>
